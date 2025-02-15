eb9bb84b4ba03a58d6ace7fd09994a1557c367d0
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-15 13:29:03 -0300

    tentativa de usar eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import parserTypeScript from "@typescript-eslint/parser";
+import pluginReact from "eslint-plugin-react";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+/** @type {import('eslint').Linter.Config[]} */
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
-    },
+    files: ["**/*.{ts,tsx}"], // Define os arquivos TypeScript a serem analisados
+    parser: parserTypeScript, // Definindo o parser para TypeScript diretamente
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      react: pluginReact,
+      "@typescript-eslint": tseslint,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
-        { allowConstantExport: true },
-      ],
+      "semi": ["error", "always"], // Força o uso de ponto e vírgula
+      "quotes": ["error", "single"], // Força o uso de aspas simples
+      ...pluginJs.configs.recommended.rules, // Regras do ESLint para JavaScript
+      ...pluginReact.configs.flat.recommended.rules, // Regras do ESLint para React
+      ...tseslint.configs.recommended.rules, // Regras do ESLint para TypeScript
     },
   },
-)
+];
