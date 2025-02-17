0da8225e7089a41f96917d585fa2059c5c625698
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-17 13:45:16 -0300

    Páginas e rotas - Finalização da Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,5 @@
 import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, Separator } from "./styles";
+import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
 
 export function Home() {
     return (
@@ -7,10 +7,10 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <input type="text" name="" id="task" />
+                    <TaskInput type="text" id="task" placeholder="Dê um nome para o seu projeto" />
 
                     <label htmlFor="minutesAmount">durante</label>
-                    <input type="number" name="" id="minutesAmount" />
+                    <MinutesAmountInput type="number" name="" id="minutesAmount" placeholder="00" />
 
                     <span>minutos.</span>
                 </FormContainer>
@@ -23,10 +23,10 @@ export function Home() {
                     <span>0</span>
                 </CountdownContainer>
 
-                <button type="submit">
+                <StartCountdownButton disabled type="submit">
                     <Play size={24} />
                     Começar
-                </button>
+                </StartCountdownButton>
             </form >
         </HomeContainer>
     )
