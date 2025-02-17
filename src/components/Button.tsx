e36ad2d8937fabb8aec76c88b106ce5d704bbf4a
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-17 11:04:07 -0300

    Páginas e rotas - React Router DOM

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer } from "./Button.style";
-
-interface ButtonProps {
-    variant?: ButtonProps;
-}
-
-export function Button({ variant = "primary" }: ButtonProps) {
-    return <ButtonContainer variant={variant}>Click me</ButtonContainer>;
-}
