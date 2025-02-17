acc52e57e2bb33b0971889227c811c60064ddbb6
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-17 11:34:23 -0300

    Páginas e rotas - Estilização do Layout

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { DefaultLayout } from './layouts/DefaultLaytout';
+import { DefaultLayout } from './layouts/DefaultLayout';
 import { History } from './pages/history';
 import { Home } from './pages/home';
 
