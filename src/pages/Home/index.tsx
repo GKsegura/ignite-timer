ad03f528cb932e356bbd7a87a2f1aa88e994047c
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-17 23:00:50 -0300

    Páginas e rotas - Aprimorando os inputs

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -7,14 +7,29 @@ export function Home() {
             <form action="">
                 <FormContainer>
                     <label htmlFor="task">Vou trabalhar em</label>
-                    <TaskInput type="text" id="task" placeholder="Dê um nome para o seu projeto" />
-
+                    <TaskInput
+                        list="task-suggestions"
+                        type="text"
+                        id="task"
+                        placeholder="Dê um nome para o seu projeto"
+                    />
+                    <datalist id="task-suggestions">
+                        <option value="Projeto 1" />
+                        <option value="Projeto 2" />
+                        <option value="Projeto 3" />
+                        <option value="Banana" />
+                    </datalist>
                     <label htmlFor="minutesAmount">durante</label>
-                    <MinutesAmountInput type="number" name="" id="minutesAmount" placeholder="00" />
-
+                    <MinutesAmountInput
+                        type="number"
+                        id="minutesAmount"
+                        placeholder="00"
+                        step={5}
+                        min={5}
+                        max={60}
+                    />
                     <span>minutos.</span>
                 </FormContainer>
-
                 <CountdownContainer>
                     <span>0</span>
                     <span>0</span>
@@ -22,7 +37,6 @@ export function Home() {
                     <span>0</span>
                     <span>0</span>
                 </CountdownContainer>
-
                 <StartCountdownButton disabled type="submit">
                     <Play size={24} />
                     Começar
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -50,6 +50,11 @@ const BaseInput = styled.input`
 
 export const TaskInput = styled(BaseInput)`
     flex: 1;
+
+    &::-webkit-calendar-picker-indicator {
+        display: none !important;
+        -webkit-appearance: none;
+    }
 `;
 
 export const MinutesAmountInput = styled(BaseInput)`
