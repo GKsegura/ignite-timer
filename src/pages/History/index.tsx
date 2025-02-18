f758cb8c8716d168231d4395b16b0f3d723ac767
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-17 23:41:20 -0300

    Páginas e rotas - Componente: Status

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,4 +1,4 @@
-import { HistoryContainer, HistoryList } from "./styles";
+import { HistoryContainer, HistoryList, Status } from "./styles";
 
 export function History() {
     return (
@@ -19,31 +19,41 @@ export function History() {
                             <td>1</td>
                             <td>2</td>
                             <td>3</td>
-                            <td>4</td>
+                            <td>
+                                <Status statusColor={"green"}>Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>1</td>
                             <td>2</td>
                             <td>3</td>
-                            <td>4</td>
+                            <td>
+                                <Status statusColor={"green"}>Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>1</td>
                             <td>2</td>
                             <td>3</td>
-                            <td>4</td>
+                            <td>
+                                <Status statusColor={"green"}>Concluído</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>1</td>
                             <td>2</td>
                             <td>3</td>
-                            <td>4</td>
+                            <td>
+                                <Status statusColor={"yellow"}>Em andamento</Status>
+                            </td>
                         </tr>
                         <tr>
                             <td>1</td>
                             <td>2</td>
                             <td>3</td>
-                            <td>4</td>
+                            <td>
+                                <Status statusColor={"red"}>Interrompido</Status>
+                            </td>
                         </tr>
                     </tbody>
                 </table>
