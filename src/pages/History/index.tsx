a745131cb1505466a12cb01133bff7b3d800c1bf
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-17 23:22:31 -0300

    Páginas e rotas - Página: History

---
+++ src/pages/History/index.tsx
@@ -0,0 +1,53 @@
+import { HistoryContainer, HistoryList } from "./styles";
+
+export function History() {
+    return (
+        <HistoryContainer>
+            <h1>Meu histórico</h1>
+            <HistoryList>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Tarefa</th>
+                            <th>Duração</th>
+                            <th>Início</th>
+                            <th>Status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                        <tr>
+                            <td>1</td>
+                            <td>2</td>
+                            <td>3</td>
+                            <td>4</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </HistoryList>
+        </HistoryContainer>
+    )
+}
