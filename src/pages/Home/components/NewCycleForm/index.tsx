5c28b730dacfb982273fedab4db6c9c8c03ba3f7
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-20 16:34:12 -0300

    Commit: Contexto no React - Contexto entre rotas

--- src/pages/Home/components/NewCycleForm/index.tsx
+++ src/pages/Home/components/NewCycleForm/index.tsx
@@ -1,14 +1,11 @@
-import { useContext } from 'react';
-
-import { useFormContext } from 'react-hook-form';
-import { CyclesContext } from '../..';
-import { FormContainer, MinutesAmountInput, TaskInput } from "./styles";
-
-
+import { useContext } from 'react'
+import { useFormContext } from 'react-hook-form'
+import { CyclesContext } from '../../../../contexts/CyclesContext'
+import { FormContainer, MinutesAmountInput, TaskInput } from './styles'
 
 export function NewCycleForm() {
-    const { activeCycle } = useContext(CyclesContext);
-    const { register } = useFormContext();
+    const { activeCycle } = useContext(CyclesContext)
+    const { register } = useFormContext()
 
     return (
         <FormContainer>
@@ -20,12 +17,14 @@ export function NewCycleForm() {
                 disabled={!!activeCycle}
                 {...register('task')}
             />
+
             <datalist id="task-suggestions">
                 <option value="Projeto 1" />
                 <option value="Projeto 2" />
                 <option value="Projeto 3" />
                 <option value="Banana" />
             </datalist>
+
             <label htmlFor="minutesAmount">durante</label>
             <MinutesAmountInput
                 type="number"
@@ -37,6 +36,8 @@ export function NewCycleForm() {
                 disabled={!!activeCycle}
                 {...register('minutesAmount', { valueAsNumber: true })}
             />
+
             <span>minutos.</span>
-        </FormContainer>)
+        </FormContainer>
+    )
 }
