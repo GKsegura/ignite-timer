6f780d6ffa28d34f9568aa51db42cfa9719b8c8d
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-24 11:02:35 -0300

    Commit: Reducers - Marcando ciclo como finalizado

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -40,29 +40,41 @@ export function CyclesContextProvider({
     children,
 }: CyclesContextProviderProps) {
     const [cyclesState, dispatch] = useReducer((state: CyclesState, action: any) => {
-        if (action.type === 'ADD_NEW_CYCLE') {
-            return {
-                ...state,
-                cycles: [...state.cycles, action.payload.newCycle],
-                activeCycleId: action.payload.newCycle.id,
-            }
-        }
 
-        if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
-            return {
-                ...state,
-                cycles: state.cycles.map((cycle) => {
-                    if (cycle.id === state.activeCycleId) {
-                        return { ...cycle, interruptedDate: new Date() }
-                    } else {
-                        return cycle
-                    }
-                }),
-                activeCycleId: null,
-            }
+        switch (action.type) {
+            case 'ADD_NEW_CYCLE':
+                return {
+                    ...state,
+                    cycles: [...state.cycles, action.payload.newCycle],
+                    activeCycleId: action.payload.newCycle.id,
+                }
+            case 'INTERRUPT_CURRENT_CYCLE':
+                return {
+                    ...state,
+                    cycles: state.cycles.map((cycle) => {
+                        if (cycle.id === state.activeCycleId) {
+                            return { ...cycle, interruptedDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null,
+                }
+            case 'MARK_CURRENT_CYCLE_AS_FINISHED':
+                return {
+                    ...state,
+                    cycles: state.cycles.map((cycle) => {
+                        if (cycle.id === state.activeCycleId) {
+                            return { ...cycle, finishedDate: new Date() }
+                        } else {
+                            return cycle
+                        }
+                    }),
+                    activeCycleId: null,
+                }
+            default:
+                return state;
         }
-
-        return state;
     }, {
         cycles: [],
         activeCycleId: null,
@@ -84,15 +96,6 @@ export function CyclesContextProvider({
                 activeCycleId,
             },
         });
-        // setCycles((state) =>
-        //     state.map((cycle) => {
-        //         if (cycle.id === activeCycleId) {
-        //             return { ...cycle, finishedDate: new Date() }
-        //         } else {
-        //             return cycle
-        //         }
-        //     }),
-        // )
     }
 
     function createNewCycle(data: CreateCycleData) {
@@ -112,7 +115,6 @@ export function CyclesContextProvider({
             },
         });
 
-        // setCycles((state) => [...state, newCycle]);
         setAmountSecondsPassed(0);
     }
 
