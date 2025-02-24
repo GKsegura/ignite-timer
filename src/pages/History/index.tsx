c2553a6ed0087cbc93cff219ca48c5359c0524e1
José Antonio Segura Marques da Silva <jose.segura.ms@gmail.com>
2025-02-24 12:14:16 -0300

    Commit: Reducers - Corrigindo listagem do histórico

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -10,9 +10,6 @@ export function History() {
     return (
         <HistoryContainer>
             <h1>Meu histórico</h1>
-
-            {/* <pre>{JSON.stringify(cycles, null, 2)}</pre> */}
-
             <HistoryList>
                 <table>
                     <thead>
@@ -29,7 +26,7 @@ export function History() {
                                 <tr key={cycle.id}>
                                     <td>{cycle.task}</td>
                                     <td>{cycle.minutesAmount} minutos</td>
-                                    <td>{formatDistanceToNow(cycle.startDate, { addSuffix: true, locale: ptBR, })}</td>
+                                    <td>{formatDistanceToNow(new Date(cycle.startDate), { addSuffix: true, locale: ptBR, })}</td>
                                     <td>
                                         {cycle.finishedDate && <Status statusColor='green'>Concluído</Status>}
                                         {cycle.interruptedDate && <Status statusColor='red'>Interrompido</Status>}
